267b0edc5e8f22940067f839536217bdd0b36a2a
sunilhn27 <sunilh.n27@gmail.com>
2023-10-20 01:51:14 +0530

    Added Loops 2

--- contracts/Basics/DynamicArrays.sol
+++ contracts/Basics/DynamicArrays.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.8.2;
 
 contract DynamicArrays {
-    uint256[6] public arr = [1, 2, 5, 6, 89];
+   uint256[] public arr = [1, 2, 5, 6, 5];
 
-    function dynamicLoop() public pure returns (uint256) {
-        uint256 data = 0;
-        for (int256 i = 0; i >= 10; i++) {
+    function dynamicLoop() public view returns (uint256) {
+        uint256 data;
+        for (uint256 i = 0; i < arr.length; i++) {
+            data += arr[i];
         }
-             
         return data;
     }
 }
