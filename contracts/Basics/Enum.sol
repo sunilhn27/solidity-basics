8c064d4b1202c01f249b7bf45b32a39d35a3c1e6
sunilhn27 <sunilh.n27@gmail.com>
2023-10-21 01:31:45 +0530

    Enums

---
+++ contracts/Basics/Enum.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.2;
+
+contract EnumExample {
+    // Define an enum named 'Status' with three constant values
+    enum Status {
+        Pending,
+        Approved,
+        Rejected
+    }
+
+    // Declare a state variable of type 'Status'
+    Status public currentStatus;
+
+    // Constructor to initialize the 'currentStatus'
+    constructor() {
+        currentStatus = Status.Pending;
+    }
+
+    // Function to change the 'currentStatus'
+    function changeStatus(Status newStatus) public {
+        currentStatus = newStatus;
+    }
+}
