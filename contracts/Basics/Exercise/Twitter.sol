dd793ffca3e4c7f4574135943f19cb26dbc6fca3
sunilhn27 <sunilh.n27@gmail.com>
2023-10-31 01:58:21 +0530

    updated tweeter contract 1

--- contracts/Basics/Exercise/Twitter.sol
+++ contracts/Basics/Exercise/Twitter.sol
@@ -4,32 +4,58 @@ pragma solidity ^0.8.0;
 
 contract Twitter {
 
-
     struct Tweet {
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
         uint256 likes;
     }
     mapping(address => Tweet[]) public tweets;
+    address public owner;
 
-    function createTweet(string memory _content) public {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER!");
+        _;
+    }
+
+
+    function createTweet(string memory _tweet) public {
         Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
             author: msg.sender,
-            content: _content,
+            content: _tweet,
             timestamp: block.timestamp,
             likes: 0
         });
-        tweets[msg.sender].push(newTweet);//tuple(address,string,uint256,uint256): 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB ,This is my First Tweet ,1698694948, 0
-    } //tuple(address,string,uint256,uint256)[]: 0x617F2E2fD72FD9D5503197092aC168c91465E7f2,This is my second Tweet,1698695062,0,0x617F2E2fD72FD9D5503197092aC168c91465E7f2,This is my second Tweet 1,1698695084,0
+        tweets[msg.sender].push(newTweet); //tuple(address,string,uint256,uint256): 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB ,This is my First Tweet ,1698694948, 0
+    }
 
-   function getTweet(address _address ,uint256 id) external view returns (Tweet memory){
-         return  tweets[_address][id];
-   }
+    function getTweet(address _address, uint256 id)
+        external
+        view
+        returns (Tweet memory)
+    {
+        return tweets[_address][id];
+    }
 
-   function getAllTweet(address _address) external view returns (Tweet[] memory){
-         return  tweets[_address];
-   }
+    function likeTweet(address author, uint256 id) external {
+        //require(tweets[author][id].id == id, "TWEET DOES NOT EXIST");
 
+        tweets[author][id].likes++;
+    }
 
+    function unlikeTweet() external {}
+
+    function getAllTweet(address _address)
+        external
+        view
+        returns (Tweet[] memory)
+    {
+        return tweets[_address];
+    }
 }
