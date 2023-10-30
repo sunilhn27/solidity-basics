cdbb78905632852420b6136ed6f5e4d8c1096101
sunilhn27 <sunilh.n27@gmail.com>
2023-10-31 01:15:34 +0530

    updated tweeter contract

--- contracts/Basics/Exercise/Twitter.sol
+++ contracts/Basics/Exercise/Twitter.sol
@@ -20,8 +20,8 @@ contract Twitter {
             timestamp: block.timestamp,
             likes: 0
         });
-        tweets[msg.sender].push(newTweet);
-    }
+        tweets[msg.sender].push(newTweet);//tuple(address,string,uint256,uint256): 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB ,This is my First Tweet ,1698694948, 0
+    } //tuple(address,string,uint256,uint256)[]: 0x617F2E2fD72FD9D5503197092aC168c91465E7f2,This is my second Tweet,1698695062,0,0x617F2E2fD72FD9D5503197092aC168c91465E7f2,This is my second Tweet 1,1698695084,0
 
    function getTweet(address _address ,uint256 id) external view returns (Tweet memory){
          return  tweets[_address][id];
