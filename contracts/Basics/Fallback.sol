6709ec9a554408d56c42c76c6da0d17090489464
sunilhn27 <sunilh.n27@gmail.com>
2023-11-07 00:55:52 +0530

    Added Fallback and receive

--- contracts/Basics/Fallback.sol
+++ contracts/Basics/Fallback.sol
@@ -6,6 +6,18 @@ pragma solidity ^0.8.0;
 contract Fallback{
 
 
-  
+     event log(string name);
+
+    fallback() external payable { //receives ETH
+        emit log("fallback");
+    }
+
+    receive() external payable { 
+        emit log("receive");
+    }
+
+    function getbal () public view returns (uint256){
+        return address(this).balance;
+    }
 
 }
