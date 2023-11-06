c14b71c5d18d21cdcd3ddfff972b2a8b6f4fa7a1
sunilhn27 <sunilh.n27@gmail.com>
2023-11-07 01:02:14 +0530

    added For loop example

--- contracts/Basics/For.sol
+++ contracts/Basics/For.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.2;
+
+contract ForLoopExample {
+
+
+    function sumArray(uint256[] memory numbers)external pure returns (uint256){
+        uint256 total = 0;
+
+        for (uint256 i = 0; i < numbers.length; i++) {
+            total += numbers[i];
+        }
+
+        return total;
+    }
+}
