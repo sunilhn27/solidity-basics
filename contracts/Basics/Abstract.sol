de91adeb77c0f851155ca523602b8cb7fa4d66b4
sunilhn27 <sunilh.n27@gmail.com>
2023-11-09 01:14:54 +0530

    update Abstract 1

--- contracts/Basics/Abstract.sol
+++ contracts/Basics/Abstract.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.2;
 
 abstract contract Repair{
 
+    string private name;
+
+    constructor(string memory _name){
+        name =_name;
+    }
 
     function repairIphone(string memory _name) external pure virtual  returns (string memory);
 
@@ -16,12 +21,11 @@ abstract contract Repair{
         return _name;
     }
 
-
-
-
 }
 
 contract MobileRepair is Repair{
+
+    constructor() Repair("Repair me"){}
 
     function repairIphone(string memory _name) external pure override  returns (string memory){
         return _name;
