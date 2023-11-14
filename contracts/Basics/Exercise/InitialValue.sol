723f21e64850ae39be8d1b1bc9c6f60236c2b988
sunilhn27 <sunilh.n27@gmail.com>
2023-11-15 00:20:37 +0530

    udapted Initial Values

--- contracts/Basics/Exercise/InitialValue.sol
+++ contracts/Basics/Exercise/InitialValue.sol
@@ -16,7 +16,24 @@ contract Values {
     }
     ActionSet public _enum; // first element 0
 
+    // reference types
+    uint256[8] public _staticArray; // a static array which all members set to their default values[0,0,0,0,0,0,0,0]
+    uint256[] public _dynamicArray; // `[]`
+    mapping(uint256 => address) public _mapping; // a mapping which all members set to their default values
+    // a struct which all members set to their default values 0, 0
+    struct Student {
+        uint256 id;
+        uint256 score;
+    }
+    Student public student;
+
     function fi() internal {} // internal blank function
 
     function fe() external {} // external blank function
+
+    bool public _bool2 = true;
+
+    function d() external {
+        delete _bool2; // delete will make _bool2 change to default(false)
+    }
 }
