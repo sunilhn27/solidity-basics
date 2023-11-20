dbf8a5705119fb2b901d6654afbb25563aae8b20
sunilhn27 <sunilh.n27@gmail.com>
2023-11-21 00:24:01 +0530

    added Import

---
+++ contracts/intermediate/Import.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.2;
+
+//import {Yeye} from './Yeye.sol';
+//import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol';
+
+
+
+
+contract Imports{
+
+    // Successfully import the Address library
+//    using Address for address;
+    // declare variable "yeye"
+    // Yeye yeye = new Yeye();
+
+    // // Test whether the function of "yeye" can be called
+    // function test() external{
+    //     yeye.hip();
+    // }
+
+}
