b7b67326e8d0e8ad292bc26088c7a3ef36905f13
sunilhn27 <sunilh.n27@gmail.com>
2023-11-23 01:01:59 +0530

    added Encode and Decode

--- contracts/intermediate/SelfDestruct.sol
+++ contracts/intermediate/SelfDestruct.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.6;
 
 contract SelfDestruct{
+
+//  The selfdestruct operation is the only way to delete a smart contract and the remaining Ether stored at that address is sent to a designated target.
+// The selfdestruct operation is designed to deal with the extreme case of contract errors. Originally the opcode was named suicide but the Ethereum community decided to rename it
+// as selfdestruct because suicide is a heavy subject and we should make every effort possible to not affect the programmer who suffer from depression.
+
+  
  
     uint public value =10;
 
