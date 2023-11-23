09d69391a02b8c183773128f80849a6f1a6c577d
sunilhn27 <sunilh.n27@gmail.com>
2023-11-24 00:52:10 +0530

    updated trycatch

--- contracts/intermediate/Trycatch.sol
+++ contracts/intermediate/Trycatch.sol
@@ -2,29 +2,46 @@
 
 pragma solidity ^0.8.2;
 
-contract TryCatch {
-    constructor(uint256 a) {
-        require(a != 0, "A is Zero");
+contract OnlyEven{
+    constructor(uint a){
+        require(a != 0, "invalid number");
         assert(a != 1);
     }
 
-    function myFun(uint256 b) public pure returns (bool succ) {
-        require(b % 2 == 0, "Not even");
-        succ = true;
+    function onlyEven(uint256 b) external pure returns(bool success){
+        require(b % 2 == 0, "Ups! Reverting");
+        success = true;
     }
 }
 
-contract OnlyEven {
-    TryCatch public trycatch;
-    event successMsg(string data);
-    event catchMsg(string data);
+contract TryCatch {
+    event SuccessEvent(string  data);
+    event CatchEvent(string message);
+    event CatchByte(bytes data);
 
-    function checkEven(uint256 c) public returns (bool succ) {
-        try trycatch.myFun(c) returns (bool success) {
-            emit successMsg("success msg");
-            return success;
-        } catch {
-            emit catchMsg("data");
+    OnlyEven even;
+
+    constructor() {
+        even = new OnlyEven(2);
+    }
+
+    function execute(uint amount) external returns (bool success) {
+        try even.onlyEven(amount) returns(bool _success){
+            emit SuccessEvent("success");
+            return _success;
+        } catch Error(string memory reason){
+            emit CatchEvent(reason);
+        }
+    }
+
+    function executeNew(uint a) external returns (bool success) {
+        try new OnlyEven(a) returns(OnlyEven _even){
+            emit SuccessEvent("success");
+            success = _even.onlyEven(a);
+        } catch Error(string memory reason) {
+            emit CatchEvent(reason);
+        } catch (bytes memory reason) {
+            emit CatchByte(reason);
         }
     }
 }
